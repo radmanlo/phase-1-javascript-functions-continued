e71ab3000c2e87249829ad0c6dfc7c5ff9ebd056
radmanlo <radman.lotfiazar@gmail.com>
2023-03-21 13:19:17 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun (activity){
+    const insideFunc = function(activity = 'roller-skate'){
+        return `This Saturday, I want to ${activity}!`
+    }
+    return insideFunc(activity)
+}
+
+function mondayWork (activity){
+    return (function(activity = 'go to the office'){
+        return `This Monday, I will ${activity}.`
+    })(activity)
+}
+
+function wrapAdjective (specialCharacter = "*" ){
+    const innerFunc = function(adjective){
+        return `You are ${specialCharacter}${adjective}${specialCharacter}!`
+    }
+    return innerFunc
+}
